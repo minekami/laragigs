6d990ad0768bb5836df0a632e52d88f63c9bf456
minekami <andrewmimura@gmail.com>
2022-10-15 12:43:35 -0700

    commit 2

---
+++ resources/views/components/flash-message.blade.php
@@ -0,0 +1,8 @@
+@if(session()->has('message'))
+    <div class="fixed top-0 transform-translate-x-1/2 bg-laravel text-white px-48 py-3">
+        <p>
+            {{session('message')}}
+        </p>
+
+    </div>
+@endif
